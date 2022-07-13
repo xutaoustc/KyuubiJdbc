53e893537be117bd3df28cbb797b487f32a60b53
xutao <xutao_ustc@163.com>
2022-07-13 20:23:08 +0800

    add max rows

--- src/main/java/sparkJdbc/Main.java
+++ src/main/java/sparkJdbc/Main.java
@@ -12,13 +12,20 @@ import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.HADOOP_SECURITY
 public class Main {
     private static String driverName = "org.apache.kyuubi.jdbc.KyuubiHiveDriver";
 
+    // https://kyuubi.apache.org/docs/r1.5.2-incubating/client/hive_jdbc.html
+    // https://kyuubi.apache.org/docs/r1.5.2-incubating/deployment/engine_share_level.html
+    // https://kyuubi.apache.org/docs/r1.5.2-incubating/deployment/spark/incremental_collection.html
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException, InterruptedException {
 
+        // hdfs/xxx@yyy /etc/security/keytabs/hdfs.keytab "jdbc:hive2://xxx:2181,xxx1:2181,xxx2:2181,xxx3:2181,xxx4:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=kyuubi_root/kyuubi?kyuubi.operation.incremental.collect=true" hdfs "select * from test.items" 100
         String principal = args[0]; // kerberos principal
         String keytab = args[1]; // keytab file location
         String kyuubiJdbcUrl = args[2];
         String user = args[3];
         String sql = args[4];
+        Integer maxRows = Integer.parseInt(args[5]);
+
+//        Connection conn = DriverManager.getConnection(kyuubiJdbcUrl, user, "");
         Configuration configuration = new Configuration();
         configuration.set(HADOOP_SECURITY_AUTHENTICATION, "kerberos");
         UserGroupInformation.setConfiguration(configuration);
@@ -31,6 +38,7 @@ public class Main {
             }
         });
         Statement st = conn.createStatement();
+        st.setMaxRows(maxRows);
         ResultSet res = st.executeQuery(sql);
         while (res.next()) {
             System.out.println(res.getString(1));
